f7c82f441f24ebc60d144e689c42b33ab611ceb9
compuTasha <mijookim.tasha@gmail.com>
2023-01-18 13:41:30 +0900

    [Setting] 프로젝트 폴더링 (#3)

---
+++ GetARock/GetARock/Global/UIComponent/SampleView.swift
@@ -0,0 +1,20 @@
+//
+//  SampleView.swift
+//  GetARock
+//
+//  Created by Mijoo Kim on 2023/01/18.
+//
+
+import UIKit
+
+class SampleView: UIView {
+
+     /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+         // Drawing code
+     }
+     */
+
+}
