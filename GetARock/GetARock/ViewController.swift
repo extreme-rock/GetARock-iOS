96b13445e01a5bb4553de615259c8fbf40a0bd50
compuTasha <mijookim.tasha@gmail.com>
2023-01-18 13:04:51 +0900

    [Add] Xcode 프로젝트 생성 (#3)

---
+++ GetARock/GetARock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetARock
+//
+//  Created by Mijoo Kim on 2023/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
