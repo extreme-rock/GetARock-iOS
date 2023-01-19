ce200fef9a6c06889b7c0dab48f9156a305a0b94
ccdkk <cdk243@gmail.com>
2023-01-20 02:06:00 +0900

    [Chore] action test를 위한 코드 추가

--- GetARock/GetARock/Global/UIComponent/SampleView.swift
+++ GetARock/GetARock/Global/UIComponent/SampleView.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class SampleView: UIView {
-
+    private let test = "test"
      /*
      // Only override draw() if you perform custom drawing.
      // An empty implementation adversely affects performance during animation.
