10ac39ededcf00780f3d06f9178975faf35a0bd0
compuTasha <mijookim.tasha@gmail.com>
2023-01-21 17:25:24 +0900

    [Style] 개행 수정 (#20)

--- GetARock/GetARock/Screens/Maps/View/MainMapViewController.swift
+++ GetARock/GetARock/Screens/Maps/View/MainMapViewController.swift
@@ -71,7 +71,9 @@ final class MainMapViewController: UIViewController {
     // MARK: - Life Cycle
     
     override func loadView() {
-        let camera: GMSCameraPosition = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 11)
+        let camera: GMSCameraPosition = GMSCameraPosition.camera(withLatitude: -33.86,
+                                                                 longitude: 151.20,
+                                                                 zoom: 11)
         let mapID = GMSMapID(identifier: Bundle.main.gmsMapID)
         
         mapView = GMSMapView(frame: .zero, mapID: mapID, camera: camera)
