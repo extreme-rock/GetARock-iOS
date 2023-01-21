ae6ec50a100b13fad7d471abed9d9e1f6b3b0d43
GODNOEL <ah2650@naver.com>
2023-01-21 20:08:59 +0900

    [Add] 폰트 추가 및 변수명 변경 (#30)

--- GetARock/GetARock/Global/Extension/UIFont+Extension.swift
+++ GetARock/GetARock/Global/Extension/UIFont+Extension.swift
@@ -8,11 +8,12 @@
 import UIKit
 
 enum FontType {
-    case largeTitle
-    case mediumTitle
-    case smallTitle
-    case headline
-    case subHeadline
+    case largeTitle01
+    case largeTitle02
+    case subTitle
+    case headline01
+    case headline02
+    case headline03
     case content
     case contentBold
     case caption
@@ -21,20 +22,34 @@ enum FontType {
 extension UIFont {
     static func setFont(_ type: FontType) -> UIFont {
         switch type {
-        case .largeTitle:
+        
+        // MARK: - Title
+        
+        case .largeTitle01:
             return systemFont(ofSize: 34, weight: .bold)
-        case .mediumTitle:
+        case .largeTitle02:
+            return systemFont(ofSize: 30, weight: .bold)
+        case .subTitle:
             return systemFont(ofSize: 18, weight: .light)
-        case .smallTitle:
-            return systemFont(ofSize: 16, weight: .light)
-        case .headline:
+        
+        // MARK: - Headline
+        
+        case .headline01:
             return systemFont(ofSize: 18, weight: .bold)
-        case .subHeadline:
+        case .headline02:
             return systemFont(ofSize: 16, weight: .bold)
+        case .headline03:
+            return systemFont(ofSize: 16, weight: .light)
+        
+        // MARK: - content
+        
         case .content:
             return systemFont(ofSize: 14, weight: .regular)
         case .contentBold:
             return systemFont(ofSize: 14, weight: .bold)
+        
+        // MARK: - content
+        
         case .caption:
             return systemFont(ofSize: 12, weight: .regular)
         }
