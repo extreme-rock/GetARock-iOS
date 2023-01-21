6c72ae7c6109f83850cb54e335616c6c00994ed9
GODNOEL <ah2650@naver.com>
2023-01-21 23:28:11 +0900

    [Add] 그라데이션 색상 구현 (#34)

--- GetARock/GetARock/Screens/Landing/View/LandingViewController.swift
+++ GetARock/GetARock/Screens/Landing/View/LandingViewController.swift
@@ -8,10 +8,41 @@
 import UIKit
 
  class LandingViewController: UIViewController {
-
+     
+     let button: UIButton = {
+         $0.setTitle("노엘천사", for: .normal)
+         return $0
+     }(UIButton())
+     
+//     let testview: UIView = {
+//         return $0
+//     }(UIView())
+     
+     override func viewDidLayoutSubviews() {
+         super.viewDidLayoutSubviews()
+         button.applyMainGradient()
+     }
+     
      override func viewDidLoad() {
          super.viewDidLoad()
+         view.addSubview(button)
+         self.button.constraint(
+            top: self.view.topAnchor,
+            leading: self.view.leadingAnchor,
+            bottom: self.view.bottomAnchor,
+            trailing: self.view.trailingAnchor,
+            padding: UIEdgeInsets(top: 200, left: 50, bottom: 200, right: 50)
+         )
 
+//         view.addSubview(testview)
+//         self.button.constraint(
+//            top: self.view.topAnchor,
+//            leading: self.view.leadingAnchor,
+//            bottom: self.view.bottomAnchor,
+//            trailing: self.view.trailingAnchor,
+//            padding: UIEdgeInsets(top: 200, left: 50, bottom: 200, right: 50)
+//         )
+         
          // Do any additional setup after loading the view.
      }
 
