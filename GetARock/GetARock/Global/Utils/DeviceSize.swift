95237532ba78359a1a2c21bc566994bae6d29681
Jay <103009135+Rookie0031@users.noreply.github.com>
2023-01-23 11:05:46 +0900

    [Design] 기본 TextField 구현을 위한 extension 및 class 추가

---
+++ GetARock/GetARock/Global/Utils/DeviceSize.swift
@@ -0,0 +1,13 @@
+//
+//  DeviceSize.swift
+//  GetARock
+//
+//  Created by 장지수 on 2023/01/23.
+//
+import UIKit
+
+struct DeviceSize {
+    static var width = UIScreen.main.bounds.width
+    static var height = UIScreen.main.bounds.height
+}
+
