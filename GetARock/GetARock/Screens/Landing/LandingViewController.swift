555f80dccf29a74c5b0c83078c2d332afc044c3b
Jay <103009135+Rookie0031@users.noreply.github.com>
2023-01-23 12:34:25 +0900

    [Add] 불필요한 파일 삭제 및 주석 작업

--- GetARock/GetARock/Screens/Landing/LandingViewController.swift
+++ GetARock/GetARock/Screens/Landing/LandingViewController.swift
@@ -7,25 +7,26 @@
 
 import UIKit
 
- class LandingViewController: BaseViewController {
-     
-     let testTextField = BasicTextField(placeholder: "테스트 용입니다")
+class LandingViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
 
-     override func viewDidLoad() {
-         super.viewDidLoad()
-//         hidekeyboardWhenTappedAround()
-         self.view.addSubview(testTextField)
-         testTextField.constraint(.widthAnchor, constant: 250)
-         testTextField.constraint(centerX: self.view.centerXAnchor, centerY: self.view.centerYAnchor)
-     }
-     /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-     }
-     */
-
- }
+// MARK: 기본 TextField 실험 코드
+//class LandingViewController: BaseViewController {
+//
+//    let testTextField = BasicTextField(placeholder: "테스트 용입니다")
+//
+//    lazy var testStackView = UIStackView(arrangedSubviews: [testTextField])
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        self.view.addSubview(testStackView)
+//
+//        testStackView.axis = .vertical
+//        testStackView.constraint(leading: self.view.leadingAnchor, trailing: self.view.trailingAnchor, padding: UIEdgeInsets(top: 0, left: 25, bottom: 0, right: 25))
+//        testStackView.constraint(centerX: self.view.centerXAnchor, centerY: self.view.centerYAnchor)
+//    }
+//}
