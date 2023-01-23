058f5c3a67576b9bc2dc79f29a77df0a0cf8dc62
Jay <103009135+Rookie0031@users.noreply.github.com>
2023-01-23 12:17:12 +0900

    [Feat] BaseController에 빈 화면 터치시 키보드 숨김 처리 추가

--- GetARock/GetARock/Global/Base/BaseViewController.swift
+++ GetARock/GetARock/Global/Base/BaseViewController.swift
@@ -8,19 +8,17 @@
 import UIKit
 
 class BaseViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        attribute()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
-    */
-
+    
+    private func attribute() {
+        view.backgroundColor = .dark01
+    }
 }
