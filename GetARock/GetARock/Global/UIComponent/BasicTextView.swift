bd4f0fdc3af8bade239ce6244656eff3579d4267
Rookie0031 <jisu7408@gmail.com>
2023-01-25 23:51:06 +0900

    [Design] BasicTextView 클래스 프로퍼티 및 레이아웃 작업

---
+++ GetARock/GetARock/Global/UIComponent/BasicTextView.swift
@@ -0,0 +1,70 @@
+//
+//  BasicTextView.swift
+//  GetARock
+//
+//  Created by Jisu Jang on 2023/01/25.
+//
+
+import UIKit
+
+class BasicTextView: UIView {
+
+    private let placeholder: String
+
+    private var maxCount: Int = 300
+
+    private lazy var textView: UITextView = {
+        // 행간 세팅
+        let style = NSMutableParagraphStyle()
+        let attributedString = NSMutableAttributedString(string: textView.text)
+        style.lineSpacing = CGFloat(7)
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: style, range: NSRange(location: 0, length: attributedString.length))
+
+        $0.attributedText = attributedString
+        $0.font = UIFont.setFont(.content)
+        $0.textColor = .white
+        $0.backgroundColor = .dark02
+
+//        $0.delegate = self
+
+        return $0
+    }(UITextView())
+
+    private lazy var countLabel = BasicLabel(contentText: "\(textView.text.count)/\(maxCount)", fontStyle: .headline01, textColorInfo: .gray02)
+
+    private lazy var placeholderLabel: UILabel = BasicLabel(contentText: placeholder, fontStyle: .content, textColorInfo: .gray02)
+
+    init(placeholder: String, maxCount: Int? = nil) {
+        self.placeholder = placeholder
+        if let maxCount { self.maxCount = maxCount }
+        super.init(frame: .zero)
+        setupLayout()
+        attribute()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func attribute() {
+        self.layer.borderWidth = 1
+        self.layer.cornerRadius = 10
+        self.layer.borderColor = UIColor.white.cgColor
+        self.backgroundColor = .dark02
+        placeholderLabel.numberOfLines = 2
+    }
+
+    private func setupLayout() {
+        self.constraint(.widthAnchor, constant: TextViewSize.width)
+        self.constraint(.heightAnchor, constant: TextViewSize.height)
+
+        self.addSubview(textView)
+        textView.constraint(top: self.topAnchor, leading: self.leadingAnchor, bottom: self.bottomAnchor, trailing: self.trailingAnchor, padding: UIEdgeInsets(top: 10, left: 10, bottom:50, right: 10))
+
+        self.addSubview(countLabel)
+        countLabel.constraint(bottom: self.bottomAnchor, trailing: self.trailingAnchor, padding: UIEdgeInsets(top: 10, left: 10, bottom: 20, right: 20))
+
+        self.addSubview(placeholderLabel)
+        placeholderLabel.constraint(top: self.topAnchor, leading: self.leadingAnchor, padding: UIEdgeInsets(top: 15, left: 20, bottom: 0, right: 0))
+    }
+}
