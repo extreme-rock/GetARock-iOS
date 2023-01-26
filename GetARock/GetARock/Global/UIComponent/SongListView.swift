f8c274dad1934488eb55573d80d1e03af66868b4
GODNOEL <ah2650@naver.com>
2023-01-26 17:44:25 +0900

    [Add] SongList 분기처리

--- GetARock/GetARock/Global/UIComponent/SongListView.swift
+++ GetARock/GetARock/Global/UIComponent/SongListView.swift
@@ -9,52 +9,84 @@ import UIKit
 
 class SongListView: UIView {
     
+    // MARK: - Property
+    
+    enum SongListType {
+        case create
+        case detail
+    }
+    
+    private var songListType: SongListType
+    
+    private let imageConfiguation = UIImage.SymbolConfiguration(pointSize: 20)
+    
     // MARK: - View
+    
     private var songTitleLabel: UILabel = {
-        $0.text = "Don't Look Back in Anger"
+        $0.text = "Don't Look Back in AngerDon't Look Back in AngerDon't Look Back in Anger"
         $0.font = UIFont.setFont(.content)
+        $0.numberOfLines = 2
         $0.textColor = .white
         return $0
     }(UILabel())
 
-    private var singerNameLabel: UILabel = {
-        $0.text = "Oasis"
+    private var artistLabel: UILabel = {
+        $0.text = "OasisDon't Look Back in AngerDon't Look Back in AngerDon't Look Back in Anger"
         $0.font = UIFont.setFont(.content)
+        $0.numberOfLines = 2
         $0.textColor = .gray02
         return $0
     }(UILabel())
     
-    private var musicIconImage: UIImageView = {
-        $0.image = UIImage(systemName: "music.quarternote.3")
+    private lazy var musicIconImage: UIImageView = {
+        $0.image = UIImage(
+            systemName: "music.quarternote.3"
+        )
         $0.contentMode = .scaleAspectFit
         $0.tintColor = .white
-        $0.setContentHuggingPriority(UILayoutPriority(rawValue: 251), for: .horizontal)
+        //muiscTextStackView 영역이 제일 길어져도뷰의 크기를 유지할 수 있게 ResistancePriority를 높임
+        $0.setContentCompressionResistancePriority(
+            UILayoutPriority(rawValue: 760),
+            for: .horizontal
+        )
         return $0
     }(UIImageView())
     
-    private let linkButton: UIButton = {
-        $0.setImage(UIImage(systemName: "arrow.up.right"), for: .normal)
+    private lazy var linkButton: UIButton = {
+        $0.setImage(UIImage(
+            systemName: "arrow.up.right",
+            withConfiguration: imageConfiguation), for: .normal
+        )
         $0.contentMode = .scaleAspectFit
         $0.tintColor = .white
-        //muiscTextStackView 영역이 제일 길어지도록 좁게 유지하고 싶은 영역의 HuggingPriority를 높임
-        $0.setContentHuggingPriority(UILayoutPriority(rawValue: 252), for: .horizontal)
+        //muiscTextStackView 영역이 제일 길어져도뷰의 크기를 유지할 수 있게 ResistancePriority를 높임
+        $0.setContentCompressionResistancePriority(
+            UILayoutPriority(rawValue: 760),
+            for: .horizontal
+        )
         return $0
     }(UIButton())
     
-    private let deleteButton: UIButton = {
-        $0.setImage(UIImage(systemName: "xmark.circle.fill"), for: .normal)
+    private lazy var deleteButton: UIButton = {
+        $0.setImage(UIImage(
+            systemName: "xmark.circle.fill",
+            withConfiguration: imageConfiguation),for: .normal
+        )
         $0.contentMode = .scaleAspectFit
         $0.tintColor = .gray02
-        //muiscTextStackView 영역이 제일 길어지도록 좁게 유지하고 싶은 영역의 HuggingPriority를 높임
-        $0.setContentHuggingPriority(UILayoutPriority(rawValue: 253), for: .horizontal)
+        //muiscTextStackView 영역이 제일 길어져도뷰의 크기를 유지할 수 있게 ResistancePriority를 높임
+        $0.setContentCompressionResistancePriority(
+            UILayoutPriority(rawValue: 760),
+            for: .horizontal
+        )
         return $0
     }(UIButton())
     
-    private lazy var muiscTextStackView: UIStackView = {
+    private lazy var songLabelStackView: UIStackView = {
         $0.axis = .vertical
         $0.spacing = 3
         return $0
-    }(UIStackView(arrangedSubviews: [songTitleLabel,singerNameLabel]))
+    }(UIStackView(arrangedSubviews: [songTitleLabel,artistLabel]))
     
     private lazy var concertedMusicStakView: UIStackView = {
         $0.backgroundColor = .dark02
@@ -69,14 +101,14 @@ class SongListView: UIView {
         $0.layer.borderColor = UIColor.gray02.cgColor
         $0.layer.borderWidth = 1
         return $0
-    }(UIStackView(arrangedSubviews: [musicIconImage,muiscTextStackView]))
+    }(UIStackView(arrangedSubviews: [musicIconImage,songLabelStackView]))
     
     // MARK: - Init
 
-    init() {
+    init(songListType: SongListType) {
+        self.songListType = songListType
         super.init(frame: .zero)
         setupLayout()
-        setupDeleteButtonLayout()
     }
     
     required init?(coder: NSCoder) {
@@ -87,26 +119,22 @@ class SongListView: UIView {
     
     private func setupLayout() {
         addSubview(concertedMusicStakView)
-//        self.concertedMusicStakView.constraint(to: concertedMusicStakView)
         self.concertedMusicStakView.constraint(.widthAnchor, constant: TextFieldSize.width)
+        setupSongListLayout()
+    }
+
+    private func setupSongListLayout() {
+        switch songListType {
+        case .create:
+            concertedMusicStakView.addArrangedSubview(deleteButton)
+        case .detail:
+            setupdetailSongListLayout(data: Song(title: "하이", artist: "오아시스", link: nil))
+        }
     }
     
-    private func setupLinkButtonLayout() {
-        concertedMusicStakView.addArrangedSubview(linkButton)
-    }
-    
-    private func setupDeleteButtonLayout() {
-        concertedMusicStakView.addArrangedSubview(deleteButton)
-    }
-    
-    private func configureLink() {
-        
-//        if 링크있으면 {
-//                setupLinkButtonLayout()
-//               }
-//
-//        if data.isLeader {
-//            setupLinkButtonLayout()
-//               }
-    }
+    private func setupdetailSongListLayout(data: Song) {
+        if data.link != nil {
+            concertedMusicStakView.addArrangedSubview(linkButton)
+        }
+    }    
 }
