9af5ac71d95e25ba8e7457c39f4889c2971ef005
ccdkk <cdk243@gmail.com>
2023-01-26 20:28:39 +0900

    [Del] LandingViewController 원상태로 (#32)

--- GetARock/GetARock/Screens/Landing/LandingViewController.swift
+++ GetARock/GetARock/Screens/Landing/LandingViewController.swift
@@ -11,18 +11,6 @@ class LandingViewController: UIViewController {
 
      override func viewDidLoad() {
          super.viewDidLoad()
-         view.backgroundColor = .black
      }
-
-
-     /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-     }
-     */
 
  }
