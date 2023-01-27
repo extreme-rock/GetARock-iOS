5f347accb6706707f8502719a9fcbc9ba6f1ab8a
ccdkk <cdk243@gmail.com>
2023-01-27 21:43:21 +0900

    [Add] gradient border 추가 (#44)

--- GetARock/GetARock/Global/UIComponent/ActiveGradationButton.swift
+++ GetARock/GetARock/Global/UIComponent/ActiveGradationButton.swift
@@ -15,61 +15,22 @@ final class ActiveGradationButton: UIButton {
         self.applyActiveGradation()
     }
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        configureUI()
+    init(borderBounds: CGRect) {
+        super.init(frame: .zero)
+        attribute(bounds: borderBounds)
     }
-    
     required init(coder: NSCoder) {
         fatalError("init(codexr:) has not been implemented")
     }
     
-    private func configureUI() {
+    private func attribute(bounds: CGRect) {
         layer.masksToBounds = true
         layer.cornerRadius = 10
         titleLabel?.font = UIFont.setFont(.contentBold)
         setTitleColor(.white, for: .normal)
-        let gradientimage = UIImage.gradientImage(bounds: CGRect(x: 0, y: 0, width: 200, height: 100), colors: [.mainPurple, .blue02])
-        let color = UIColor(patternImage: gradientimage)
+        let gradientImage = UIImage.gradientImage(bounds: bounds, colors: [.mainPurple, .blue02])
+        let color = UIColor(patternImage: gradientImage)
         layer.borderColor = color.cgColor
         layer.borderWidth = 1
-        
-        let gradientimage1 = UIImage.gradientImage(bounds: CGRect(x: 0, y: 0, width: 200, height: 100), colors: [.activeGradationPurple, .black])
-        let color1 = UIColor(patternImage: gradientimage1)
-//        backgroundColor = .clear
-    }
-}
-
-
-
-class View: UIView {
-    
-    init(frame: CGRect, cornerRadius: CGFloat, colors: [UIColor], lineWidth: CGFloat = 5) {
-        super.init(frame: frame)
-        
-        self.layer.cornerRadius = cornerRadius
-        self.layer.masksToBounds = true
-        let gradient = CAGradientLayer()
-        gradient.frame = CGRect(origin: CGPoint.zero, size: self.frame.size)
-        gradient.colors = colors.map({ (color) -> CGColor in
-            color.cgColor
-        })
-        
-        gradient.startPoint = CGPoint(x: 0, y: 1)
-        gradient.endPoint = CGPoint(x: 1, y: 1)
-        
-        let shape = CAShapeLayer()
-        shape.lineWidth = lineWidth
-        shape.path = UIBezierPath(roundedRect: self.bounds.insetBy(dx: lineWidth,
-                                                                   dy: lineWidth), cornerRadius: cornerRadius).cgPath
-        shape.strokeColor = UIColor.black.cgColor
-        shape.fillColor = UIColor.clear.cgColor
-        gradient.mask = shape
-        
-        self.layer.addSublayer(gradient)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
--- GetARock/GetARock/Screens/Landing/LandingViewController.swift
+++ GetARock/GetARock/Screens/Landing/LandingViewController.swift
@@ -9,9 +9,7 @@ import UIKit
 
  class LandingViewController: UIViewController {
 
-     let test = ActiveGradationButton()
-     let testview = UIView()
-     let myView = View(frame: CGRect(x: 0, y: 0, width: 200, height: 100), cornerRadius: 25, colors: [UIColor.red, .orange, .yellow], lineWidth: 2)
+     let test = ActiveGradationButton(borderBounds: CGRect(x: 0, y: 0, width: 200, height: 50))
          
      override func viewDidLoad() {
          super.viewDidLoad()
@@ -21,40 +19,5 @@ import UIKit
          test.constraint( centerX: view.centerXAnchor, centerY: view.centerYAnchor)
          test.constraint(.widthAnchor, constant: 200)
          test.constraint(.heightAnchor, constant: 50)
-
-//         view.addSubview(myView)
-//         myView.constraint(centerX: view.centerXAnchor, centerY: view.centerYAnchor)
-//         myView.constraint(.widthAnchor, constant: 200)
-//         myView.constraint(.heightAnchor, constant: 100)
-//         myView.addSubview(test)
-//         test.constraint(centerX: myView.centerXAnchor,
-//                         centerY: myView.centerYAnchor)
-//         test.constraint(.widthAnchor, constant: 200)
-//         test.constraint(.heightAnchor, constant: 100)
-//         let action = UIAction { _ in
-//             print("hi")
-//         }
-//         test.addAction(action, for: .touchUpInside)
-//         view.addSubview(testview)
-//         testview.constraint( centerX: view.centerXAnchor, centerY: view.centerYAnchor)
-//         testview.constraint(.widthAnchor, constant: 200)
-//         testview.constraint(.heightAnchor, constant: 50)
-//         let gradient = UIImage.gradientImage(bounds: testview.bounds, colors: [.systemBlue, .systemRed])
-//         let gradientColor = UIColor(patternImage: gradient)
-//         testview.layer.borderColor = gradientColor.cgColor
-//         testview.layer.borderWidth = 3
-         // Do any additional setup after loading the view.
      }
-
-
-     /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-     }
-     */
-
  }
