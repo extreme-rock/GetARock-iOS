e01415ebd24a5936a364589f4da8ed59bc5ef62a
ccdkk <cdk243@gmail.com>
2023-01-28 11:22:53 +0900

    [Del] 테스트코드 삭제 (#45)

--- GetARock/GetARock/Screens/Landing/LandingViewController.swift
+++ GetARock/GetARock/Screens/Landing/LandingViewController.swift
@@ -8,18 +8,8 @@
 import UIKit
 
 class LandingViewController: BaseViewController {
-    
-    let test = SelectCollectionView(widthState: .fixedWidth, items: ["20대 미만","20 대", "60대 이상", "30 대", "40 대"], widthSize: 100)
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
-        view.addSubview(test)
-        
-        test.constraint(top: view.topAnchor,
-                        leading: view.leadingAnchor,
-                        bottom: view.bottomAnchor,
-                        trailing: view.trailingAnchor,
-                        padding: UIEdgeInsets(top: 300, left: 10, bottom: 0, right: 0))
-        
     }
 }
