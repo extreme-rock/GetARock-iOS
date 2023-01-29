a047ffa6eaae69e43e72b92691efcddef282f97c
ccdkk <cdk243@gmail.com>
2023-01-30 01:24:18 +0900

    [Del] SampleView 원상복귀 (#44)

--- GetARock/GetARock/Global/UIComponent/SampleView.swift
+++ GetARock/GetARock/Global/UIComponent/SampleView.swift
@@ -13,7 +13,7 @@ class SampleView: UIView {
      // Only override draw() if you perform custom drawing.
      // An empty implementation adversely affects performance during animation.
      override func draw(_ rect: CGRect) {
-         // Drawing 1code
+         // Drawing code
      }
      */
 
