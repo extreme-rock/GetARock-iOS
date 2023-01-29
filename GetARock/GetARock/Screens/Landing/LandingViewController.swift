4ddfbbe22923a7d0526b3205f97730faf45b1802
ccdkk <cdk243@gmail.com>
2023-01-29 18:22:22 +0900

    [Feat] 버튼의 터치영역 확대 (#55) - 버튼의 터치영역이 Label에 한정되어서 Label의 constraint를 확대하는 방법으로 터치영역을 확대했습니다.

--- GetARock/GetARock/Screens/Landing/LandingViewController.swift
+++ GetARock/GetARock/Screens/Landing/LandingViewController.swift
@@ -7,23 +7,26 @@
 
 import UIKit
 
- class LandingViewController: UIViewController {
-
-     override func viewDidLoad() {
-         super.viewDidLoad()
-
-         // Do any additional setup after loading the view.
-     }
-
-
-     /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-     }
-     */
-
- }
+class LandingViewController: UIViewController {
+    
+    lazy var segment = DarkSegmentedControl(items: ["내밴드", "내 정보"])
+    let segmnet2 = MainPurpleSegmentedControl(buttonTitles: ["내밴드", "내 정보", "난나나"])
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(segment)
+        segment.constraint(top: view.topAnchor,
+                           leading: view.leadingAnchor,
+                           trailing: view.trailingAnchor,
+                           padding: UIEdgeInsets(top: 300, left: 20, bottom: 0, right: 20))
+        segment.constraint(.heightAnchor, constant: 50)
+        
+        view.addSubview(segmnet2)
+        segmnet2.constraint(top: segment.bottomAnchor,
+                            leading: view.leadingAnchor,
+                            trailing: view.trailingAnchor,
+                            padding: UIEdgeInsets(top: 50, left: 20, bottom: 0, right: 20))
+        segmnet2.constraint(.heightAnchor, constant: 60)
+       
+    }
+}
