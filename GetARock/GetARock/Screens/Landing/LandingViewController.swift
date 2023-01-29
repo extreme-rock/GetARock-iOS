fa8eaa3927467ce76213540a51174180a7ce526a
ccdkk <cdk243@gmail.com>
2023-01-30 01:22:12 +0900

    [Del] 테스트코드 삭제 (#55)

--- GetARock/GetARock/Screens/Landing/LandingViewController.swift
+++ GetARock/GetARock/Screens/Landing/LandingViewController.swift
@@ -8,33 +8,8 @@
 import UIKit
 
 class LandingViewController: UIViewController {
-    
-    lazy var segment = DarkSegmentedControl(items: ["내밴드", "내 정보"])
-    let segmnet2 = MainPurpleSegmentedControl(buttonTitles: ["내밴드", "내 정보", "난나나"])
-    let segment3 = UnderlineSegmentedControl(items: ["내밴드", "내 정보", "난나나"])
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.addSubview(segment)
-        segment.constraint(top: view.topAnchor,
-                           leading: view.leadingAnchor,
-                           trailing: view.trailingAnchor,
-                           padding: UIEdgeInsets(top: 300, left: 20, bottom: 0, right: 20))
-        segment.constraint(.heightAnchor, constant: 50)
-        
-        view.addSubview(segmnet2)
-        segmnet2.constraint(top: segment.bottomAnchor,
-                            leading: view.leadingAnchor,
-                            trailing: view.trailingAnchor,
-                            padding: UIEdgeInsets(top: 50, left: 20, bottom: 0, right: 20))
-        segmnet2.constraint(.heightAnchor, constant: 60)
-        
-        view.addSubview(segment3)
-        segment3.constraint(top: segmnet2.bottomAnchor,
-                            leading: view.leadingAnchor,
-                            trailing: view.trailingAnchor,
-                            padding: UIEdgeInsets(top: 50, left: 20, bottom: 0, right: 20))
-        segment3.constraint(.heightAnchor, constant: 60)
-       
     }
 }
