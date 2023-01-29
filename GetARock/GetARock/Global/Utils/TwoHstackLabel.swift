0e005b87668a65a07818d73491fc177df61f3394
Jay <103009135+Rookie0031@users.noreply.github.com>
2023-01-29 13:03:02 +0900

    [Design] 중복 확인 레이블 관련 코드 추가

---
+++ GetARock/GetARock/Global/Utils/TwoHstackLabel.swift
@@ -0,0 +1,29 @@
+//
+//  TwoHstackLabel.swift
+//  GetARock
+//
+//  Created by 장지수 on 2023/01/29.
+//
+import UIKit
+
+struct TwoHstackLabel {
+    // 텍스트 필드 내 텍스트를 중복 확인 검사하는 경우 하단에 표기되는 레이블
+    static var checkLabel: UIStackView = {
+        let imageView: UIImageView = {
+            let imageView = UIImageView()
+            imageView.image = UIImage(systemName: "checkmark.circle")!
+            imageView.tintColor = .systemBlue
+            imageView.constraint(.widthAnchor, constant: 20)
+            imageView.constraint(.heightAnchor, constant: 20)
+            return imageView
+        }()
+        
+        let label = BasicLabel(contentText: "사용 가능해요", fontStyle: .caption, textColorInfo: .systemBlue)
+        
+        let stackView = UIStackView(arrangedSubviews: [imageView, label])
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        return stackView
+    }()
+}
+
