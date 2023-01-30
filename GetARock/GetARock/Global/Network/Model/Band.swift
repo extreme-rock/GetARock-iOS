e758af91bc76872085b88710ad74ed5c89098bc0
compuTasha <mijookim.tasha@gmail.com>
2023-01-30 11:27:19 +0900

    [Add] 더미데이터 struct 형태로 변경 (#38)

--- GetARock/GetARock/Global/Network/Model/Band.swift
+++ GetARock/GetARock/Global/Network/Model/Band.swift
@@ -18,20 +18,22 @@ struct Band {
 //    let sns: [SNS]?
 }
 
-var testBands: [Band] = [
-    Band(name: "레드제플린", location: Location(
-        name: "레드제플린",
-        address: "대구광역시 남구 명덕로 102",
-        additionalAddress: "레드제플린",
-        coordinate: Coordinate(latitude: 35.85672, longitude: 128.58130))),
-    Band(name: "블랙로즈블랙로즈블랙", location: Location(
-        name: "블랙로즈",
-        address: "대한민국 대구광역시 중구 태평로2가 17-11",
-        additionalAddress: nil,
-        coordinate: Coordinate(latitude: 35.87478, longitude: 128.59192))),
-    Band(name: "모닝샐러드", location: Location(
-        name: "모닝샐러드",
-        address: "대한민국 대구광역시 중구 남산동 2185-110",
-        additionalAddress: nil,
-        coordinate: Coordinate(latitude: 35.85785, longitude: 128.58661))),
-]
+struct BandDummyData {
+    static let testBands: [Band] = [
+        Band(name: "레드제플린", location: Location(
+            name: "레드제플린",
+            address: "대구광역시 남구 명덕로 102",
+            additionalAddress: "레드제플린",
+            coordinate: Coordinate(latitude: 35.85672, longitude: 128.58130))),
+        Band(name: "블랙로즈블랙로즈블랙", location: Location(
+            name: "블랙로즈",
+            address: "대한민국 대구광역시 중구 태평로2가 17-11",
+            additionalAddress: nil,
+            coordinate: Coordinate(latitude: 35.87478, longitude: 128.59192))),
+        Band(name: "모닝샐러드", location: Location(
+            name: "모닝샐러드",
+            address: "대한민국 대구광역시 중구 남산동 2185-110",
+            additionalAddress: nil,
+            coordinate: Coordinate(latitude: 35.85785, longitude: 128.58661))),
+    ]
+}
--- GetARock/GetARock/Global/Network/Model/Event.swift
+++ GetARock/GetARock/Global/Network/Model/Event.swift
@@ -19,20 +19,22 @@ struct Event {
 //    let band: Band
 }
 
-var testEvents: [Event] = [
-    Event(name: "이벤트1", location: Location(
-        name: "밴드1",
-        address: "대한민국 대구광역시 중구 대신동 291-61",
-        additionalAddress: "2층",
-        coordinate: Coordinate(latitude: 35.86677, longitude: 128.57930))),
-    Event(name: "이벤트2", location: Location(
-        name: "밴드2",
-        address: "대구광역시 수성구 달구벌대로 2354",
-        additionalAddress: "시카고 라이브클럽",
-        coordinate: Coordinate(latitude: 35.85922, longitude: 128.61990))),
-    Event(name: "이벤트3", location: Location(
-        name: "밴드3",
-        address: "대한민국 대구광역시 남구 대명동 1883-3",
-        additionalAddress: "오투뮤직스토리 지하",
-        coordinate: Coordinate(latitude: 35.85388, longitude: 128.58477))),
-]
+struct EventDummyData {
+    static let testEvents: [Event] = [
+        Event(name: "이벤트1", location: Location(
+            name: "밴드1",
+            address: "대한민국 대구광역시 중구 대신동 291-61",
+            additionalAddress: "2층",
+            coordinate: Coordinate(latitude: 35.86677, longitude: 128.57930))),
+        Event(name: "이벤트2", location: Location(
+            name: "밴드2",
+            address: "대구광역시 수성구 달구벌대로 2354",
+            additionalAddress: "시카고 라이브클럽",
+            coordinate: Coordinate(latitude: 35.85922, longitude: 128.61990))),
+        Event(name: "이벤트3", location: Location(
+            name: "밴드3",
+            address: "대한민국 대구광역시 남구 대명동 1883-3",
+            additionalAddress: "오투뮤직스토리 지하",
+            coordinate: Coordinate(latitude: 35.85388, longitude: 128.58477))),
+    ]
+}
