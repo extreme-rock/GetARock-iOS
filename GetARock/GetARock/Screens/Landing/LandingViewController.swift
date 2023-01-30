c4e1b16ddc8161df4d24a521835494430c993f50
ccdkk <cdk243@gmail.com>
2023-01-30 11:05:04 +0900

    [Add] LandingView 원복 (#45)

--- GetARock/GetARock/Screens/Landing/LandingViewController.swift
+++ GetARock/GetARock/Screens/Landing/LandingViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class LandingViewController: BaseViewController {
+class LandingViewController: UIViewController {
 
      override func viewDidLoad() {
          super.viewDidLoad()
