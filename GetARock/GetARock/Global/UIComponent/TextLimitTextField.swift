606e843bd7ddf73d980548136fd5fd1ee97a87b4
Jay <103009135+Rookie0031@users.noreply.github.com>
2023-01-31 20:01:11 +0900

    [Add] 중복 확인 버튼 숨김처리 관련 코드 추가 + 주석 작업

--- GetARock/GetARock/Global/UIComponent/TextLimitTextField.swift
+++ GetARock/GetARock/Global/UIComponent/TextLimitTextField.swift
@@ -9,15 +9,20 @@ import UIKit
 enum DuplicationCheckType {
     case userNickName
     case bandName
+    case none
 }
 
 final class TextLimitTextField: UIView {
+    
+    // MARK: - Property
     
     private let placeholder: String
     
     private let maxCount: Int
 
-    private let duplicationCheckType: DuplicationCheckType
+    private let type: DuplicationCheckType
+    
+    // MARK: - View
     
     private lazy var textField: UITextField = {
         $0.addTarget(self, action: #selector(textFieldTextDidChange), for: .editingChanged)
@@ -29,18 +34,23 @@ final class TextLimitTextField: UIView {
         return $0
     }(DefaultButton(frame: CGRect(x: 0, y: 0, width: 80, height: 40)))
     
-    init(placeholer: String, maxCount: Int, checkCase: DuplicationCheckType) {
+    // MARK: - Life Cycle
+    
+    init(placeholer: String, maxCount: Int, checkType: DuplicationCheckType) {
         self.maxCount = maxCount
         self.placeholder = placeholer
-        self.duplicationCheckType = checkCase
+        self.type = checkType
         super.init(frame: .zero)
         
         setupLayout()
+        attribute()
     }
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - Method
     
     private func setupLayout() {
         self.constraint(.widthAnchor, constant: TextFieldSize.width)
@@ -56,6 +66,12 @@ final class TextLimitTextField: UIView {
             padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10))
         checkButton.constraint(.widthAnchor, constant: 80)
         checkButton.constraint(.heightAnchor, constant: 40)
+    }
+    
+    private func attribute() {
+        if type = .none {
+            checkButton.isHidden = true
+        }
     }
 }
 
