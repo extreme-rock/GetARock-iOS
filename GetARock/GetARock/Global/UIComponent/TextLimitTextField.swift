def5af00fe9f4ec69aaf4fb4202854fe00955bfe
Rookie_ki_ki <jisu7408@gmail.com>
2023-02-02 12:54:22 +0900

    [Fix] 중복체크 레이블이 띄워지지않는 문제 해결

--- GetARock/GetARock/Global/UIComponent/TextLimitTextField.swift
+++ GetARock/GetARock/Global/UIComponent/TextLimitTextField.swift
@@ -12,7 +12,7 @@ enum DuplicationCheckType {
     case none
 }
 
-final class TextLimitTextField: UIStackView {
+final class TextLimitTextField: UIView {
     
     // MARK: - Property
     
@@ -34,11 +34,29 @@ final class TextLimitTextField: UIStackView {
     
     private lazy var checkButton: DefaultButton = {
         $0.setTitle("중복 확인", for: .normal)
-        $0.addTarget(self, action: #selector(didTapCheckButton), for: .touchUpInside)
+        $0.tag = (1...10).randomElement()!
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapCheckButton))
+        $0.addGestureRecognizer(tapGesture)
         return $0
     }(DefaultButton(frame: CGRect(x: 0, y: 0, width: 80, height: 40)))
-    
-    private let duplicationCheckLabel: UIStackView = TwoConsecutiveLabel.checkLabel
+
+    private lazy var duplicationCheckLabel: UIStackView = {
+        let imageView: UIImageView = {
+            $0.image = UIImage(systemName: "checkmark.circle")!
+            $0.tintColor = .systemBlue
+            $0.constraint(.widthAnchor, constant: 20)
+            $0.constraint(.heightAnchor, constant: 20)
+            return $0
+        }(UIImageView())
+
+        let label = BasicLabel(contentText: "사용 가능해요", fontStyle: .caption, textColorInfo: .systemBlue)
+
+        $0.axis = .horizontal
+        $0.spacing = 5
+        $0.addArrangedSubview(imageView)
+        $0.addArrangedSubview(label)
+        return $0
+    }(UIStackView(frame: .zero))
     
     // MARK: - Life Cycle
     
@@ -51,6 +69,9 @@ final class TextLimitTextField: UIStackView {
         
         setupLayout()
         attribute()
+
+        print("Init 과정에서 버튼 태그 체크")
+        print(self.checkButton.tag)
     }
     
     required init(coder: NSCoder) {
@@ -60,11 +81,11 @@ final class TextLimitTextField: UIStackView {
     // MARK: - Method
     
     private func setupLayout() {
-        textField.constraint(.widthAnchor, constant: TextFieldSize.width)
-        textField.constraint(.heightAnchor, constant: TextFieldSize.height)
-        
-        self.addArrangedSubview(textField)
-        self.addArrangedSubview(duplicationCheckLabel)
+        self.constraint(.widthAnchor, constant: TextFieldSize.width)
+        self.constraint(.heightAnchor, constant: TextFieldSize.height)
+
+        self.addSubview(textField)
+        textField.constraint(to: self)
 
         self.addSubview(checkButton)
         checkButton.constraint(
@@ -73,15 +94,16 @@ final class TextLimitTextField: UIStackView {
             padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10))
         checkButton.constraint(.widthAnchor, constant: 80)
         checkButton.constraint(.heightAnchor, constant: 40)
+
+        self.addSubview(duplicationCheckLabel)
+        duplicationCheckLabel.constraint(top: textField.bottomAnchor, leading: textField.leadingAnchor, padding: UIEdgeInsets(top: 5, left: 0, bottom: 0, right: 2))
     }
     
     private func attribute() {
         if type == .none {
             checkButton.isHidden = true
         }
-        duplicationCheckLabel.isHidden = true
-        self.axis = .vertical
-        self.spacing = 5
+        self.duplicationCheckLabel.isHidden = true
     }
 }
 
@@ -101,6 +123,11 @@ extension TextLimitTextField {
     
     //MARK: 중복 확인 버튼
     @objc func didTapCheckButton() {
+        print("버튼 태그")
+        print(self.checkButton.tag)
+
+        // 1번 버튼을 클릭하면 얘가 수행되지않고
+        // 2번 버튼을 클릭하면 여기 까지 모두 수행됨 vice versa
         Task {
             do {
                 if textField.text == "모여락" {
@@ -116,15 +143,16 @@ extension TextLimitTextField {
             }
         }
     }
+
     
     private func showDuplicationCheckLabel(with isChecked: Bool) {
-        duplicationCheckLabel.isHidden = false
+        self.duplicationCheckLabel.isHidden = false
         let imageView = duplicationCheckLabel.arrangedSubviews.first! as! UIImageView
         imageView.image = isChecked ? UIImage(systemName: "checkmark.circle")! : UIImage(systemName: "x.circle")!
         imageView.tintColor = isChecked ? .systemBlue : .systemRed
         
         let label = duplicationCheckLabel.arrangedSubviews.last! as! UILabel
-        label.text = isChecked ? "가능합니다" : "불가능합니다"
+        label.text = isChecked ? "사용 가능해요" : "이미 사용하고 있어요"
         label.textColor = isChecked ? .systemBlue : .systemRed
     }
 }
