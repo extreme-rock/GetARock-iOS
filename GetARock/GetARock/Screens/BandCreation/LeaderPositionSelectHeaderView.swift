f1ce78269e8df00ada90d31f7378fb6d5a37c5b7
Jay <103009135+Rookie0031@users.noreply.github.com>
2023-02-12 20:44:34 +0900

    [Add] 리더 포지션 선택 UI 구현 및 관련 파일 폴더링

---
+++ GetARock/GetARock/Screens/BandCreation/LeaderPositionSelectHeaderView.swift
@@ -0,0 +1,50 @@
+//
+//  LeaderPositionSelectHeaderView.swift
+//  GetARock
+//
+//  Created by 장지수 on 2023/02/12.
+//
+
+import UIKit
+
+final class LeaderPositionSelectHeaderView: UIView {
+    
+    private let pageIndicatorLabel = BasicLabel(contentText: "1/3", fontStyle: .subTitle, textColorInfo: .gray02)
+    
+    private let titleLabel: BasicLabel = {
+        $0.numberOfLines = 3
+        return $0
+    }(BasicLabel(contentText: "00님의\n밴드에서포지션을\n알려주세요.", fontStyle: .largeTitle01, textColorInfo: .white))
+    
+    private let subTitleLabel = BasicLabel(contentText: "최대 2개까지 선택 가능합니다.", fontStyle: .subTitle, textColorInfo: .gray02)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        self.addSubview(pageIndicatorLabel)
+        pageIndicatorLabel.constraint(top: self.topAnchor,
+                                      leading: self.leadingAnchor,
+                                      trailing: self.trailingAnchor,
+                                      padding: UIEdgeInsets(top: 20, left: 17, bottom: 0, right: 0))
+        
+        self.addSubview(titleLabel)
+        titleLabel.constraint(top: pageIndicatorLabel.bottomAnchor,
+                              leading: self.leadingAnchor,
+                              trailing: self.trailingAnchor,
+                              padding: UIEdgeInsets(top: 6, left: 16, bottom: 0, right: 16))
+        
+        self.addSubview(subTitleLabel)
+        subTitleLabel.constraint(top: titleLabel.bottomAnchor,
+                                 leading: self.leadingAnchor,
+                                 bottom: self.bottomAnchor,
+                                 trailing: self.trailingAnchor,
+                                 padding: UIEdgeInsets(top: 10, left: 16, bottom: 49, right: 16))
+    }
+}
---
+++ GetARock/GetARock/Screens/BandCreation/LeaderPositionSelectViewController.swift
@@ -0,0 +1,117 @@
+//
+//  LeaderPositionSelectViewController.swift
+//  GetARock
+//
+//  Created by 장지수 on 2023/02/12.
+//
+
+import UIKit
+
+final class LeaderPositionSelectViewController: UIViewController {
+    
+    private var positions: [Item] = [
+        .position(Position(instrumentName: "보컬", instrumentImageName: .vocal, isETC: false)),
+        .position(Position(instrumentName: "기타", instrumentImageName: .guitar, isETC: false)),
+        .position(Position(instrumentName: "키보드", instrumentImageName: .keyboard, isETC: false)),
+        .position(Position(instrumentName: "드럼", instrumentImageName: .drum, isETC: false)),
+        .position(Position(instrumentName: "베이스", instrumentImageName: .bass, isETC: false)),
+        .plusPosition
+    ]
+    
+    private lazy var positionCollectionView = PositionCollectionView(
+        cellType: .position,
+        items: positions,
+        isNeedHeader: true,
+        headerView: LeaderPositionSelectHeaderView()
+    )
+    
+    private let nextButton: BottomButton = {
+        $0.setTitle("다음", for: .normal)
+        return $0
+    }(BottomButton())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLayout()
+        attribute()
+        configureDelegate()
+        addObservePositionPlusButtonTapped()
+        addObservePositionDeleteButtonTapped()
+    }
+    
+    private func attribute() {
+        self.view.backgroundColor = .dark01
+    }
+    
+    private func configureDelegate() {
+        positionCollectionView.delegate = self
+    }
+    
+    private func addObservePositionPlusButtonTapped() {
+         NotificationCenter.default.addObserver(self,
+                                                selector: #selector(showPositionPlusModal),
+                                                name: Notification.Name(StringLiteral.showPositionPlusModal),
+                                                object: nil)
+     }
+    
+    @objc
+    private func showPositionPlusModal() {
+        let viewController = PlusPositionViewController()
+        viewController.delegate = self
+        present(viewController, animated: true)
+    }
+    
+    private func addObservePositionDeleteButtonTapped() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(deletePosition(_:)),
+                                               name: Notification.Name(StringLiteral.deletePositionCell),
+                                               object: nil)
+    }
+    
+    @objc
+    private func deletePosition(_ notification: Notification) {
+        guard let deleteIndexPath = notification.userInfo?["index"] as? Int else { return }
+        positions.remove(at: deleteIndexPath)
+        self.positionCollectionView.applySnapshot(with: positions)
+        self.updateDataSourceIndex(from: deleteIndexPath, endIndex: positions.count - 1)
+    }
+    
+    private func updateDataSourceIndex(from startIndex: Int, endIndex: Int) {
+        for index in startIndex..<endIndex {
+            positionCollectionView.updateCellIndex(at: IndexPath(item: index, section: 0))
+        }
+    }
+    
+    private func setupLayout() {
+        self.view.addSubview(positionCollectionView)
+        self.view.addSubview(nextButton)
+        
+        positionCollectionView.constraint(top: view.safeAreaLayoutGuide.topAnchor,
+                                          leading: view.leadingAnchor,
+                                          bottom: nextButton.topAnchor,
+                                          trailing: view.trailingAnchor,
+                                          padding: UIEdgeInsets(top: 10, left: 16, bottom: 10, right: 16))
+        
+        
+        nextButton.constraint(bottom: view.bottomAnchor,
+                              centerX: view.centerXAnchor,
+                              padding: UIEdgeInsets(top: 0, left: 0, bottom: 42, right: 0))
+    }
+}
+
+extension LeaderPositionSelectViewController: PositionCollectionViewDelegate {
+    func canSelectPosition(_ collectionView: UICollectionView, indexPath: IndexPath, selectedItemsCount: Int) -> Bool {
+        return true
+    }
+}
+
+extension LeaderPositionSelectViewController: PlusPositionViewControllerDelegate {
+    func addPosition(instrumentName: String) {
+        let newPosition: Item = .position(Position(instrumentName: instrumentName,
+                                                   instrumentImageName: .etc,
+                                                   isETC: true))
+        positions.insert(newPosition, at: positions.count - 1)
+        self.positionCollectionView.applySnapshot(with: positions)
+    }
+}
+
