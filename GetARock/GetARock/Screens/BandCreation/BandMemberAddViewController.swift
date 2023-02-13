156fd4dbc4bb33a477d7bd300b2030e9a3bffdee
Jay <103009135+Rookie0031@users.noreply.github.com>
2023-02-13 20:59:24 +0900

    [Add] 밴드 구성원을 추가하는 View Controller 파일 추가 (#82)

---
+++ GetARock/GetARock/Screens/BandCreation/BandMemberAddViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BandMemberAddViewController.swift
+//  GetARock
+//
+//  Created by 장지수 on 2023/02/13.
+//
+
+import UIKit
+
+class BandMemberAddViewController: UIViewController {
+
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         attribute()
+     }
+    
+    private func attribute() {
+        self.view.backgroundColor = .white
+    }
+
+ }
+
