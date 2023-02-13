5c509f9e51daa88b25619df82fb070815c9edb3d
GODNOEL <ah2650@naver.com>
2023-02-13 18:39:57 +0900

    [Resource] 상세에 사용되는 customPinIcon 추가 (#79)

--- GetARock/GetARock/Global/Literal/ImageLiteral.swift
+++ GetARock/GetARock/Global/Literal/ImageLiteral.swift
@@ -26,6 +26,7 @@ enum ImageLiteral {
     static var youtubeIcon: UIImage { .load(named: "youtubeIcon") }
     static var instagramIcon: UIImage { .load(named: "instagramIcon") }
     static var soundCloudIcon: UIImage { .load(named: "soundCloudIcon") }
+    static var customPinIcon: UIImage { .load(named: "customPinIcon") }
 }
 
 extension UIImage {
