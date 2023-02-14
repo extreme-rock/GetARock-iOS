83403eb4eef24c56ac5fdb128c19df7f3179d7d4
GODNOEL <ah2650@naver.com>
2023-02-14 21:43:59 +0900

    [Add] String -> Date로 변환 메소드 추가 (#84)

--- GetARock/GetARock/Global/Extension/String+Extension.swift
+++ GetARock/GetARock/Global/Extension/String+Extension.swift
@@ -7,25 +7,37 @@
 
 import Foundation
 
-//MARK: 특수문자 제한 정규표현식
-
 extension String {
+    
+    // MARK: 특수문자 제한 정규표현식
     func checkStringExpression() -> Bool{
-           do {
-               let regex = try NSRegularExpression(
+        do {
+            let regex = try NSRegularExpression(
                 pattern: "^[a-zA-Z0-9가-힣ㄱ-ㅎㅏ-ㅣ\\s]$",
                 options: .caseInsensitive)
-
-               if let _ = regex.firstMatch(
+            
+            if let _ = regex.firstMatch(
                 in: self,
                 options: NSRegularExpression.MatchingOptions.reportCompletion,
                 range: NSMakeRange(0, self.count)){
-                   return true
-               }
-           } catch {
-               print(error)
-               return false
-           }
-           return false
-       }
+                return true
+            }
+        } catch {
+            print(error)
+            return false
+        }
+        return false
+    }
+    
+    // MARK: String -> Date로 변환
+    func toDate() -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.timeZone = TimeZone(identifier: "UTC")
+        if let date = dateFormatter.date(from: self) {
+            return date
+        } else {
+            return nil
+        }
+    }
 }
