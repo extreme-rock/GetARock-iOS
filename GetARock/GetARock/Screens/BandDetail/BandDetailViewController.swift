4761dd4a81bf0a400e96f143feef5909fadf1a40
GODNOEL <ah2650@naver.com>
2023-02-14 16:29:50 +0900

    [Design] AutoLayout 값 수정 (#79)

--- GetARock/GetARock/Screens/BandDetail/BandDetailViewController.swift
+++ GetARock/GetARock/Screens/BandDetail/BandDetailViewController.swift
@@ -22,7 +22,7 @@ final class BandDetailViewController: UIViewController {
         bandTopInfoView.constraint(top: self.view.topAnchor,
                         leading: self.view.leadingAnchor,
                         trailing: self.view.trailingAnchor,
-                        padding: UIEdgeInsets(top: 50, left: 0, bottom: 0, right: 0))
+                        padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0))
         
         view.addSubview(bandDetailContentView)
         bandDetailContentView.constraint(top: bandTopInfoView.bottomAnchor,
