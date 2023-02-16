fd1e51216ddd08ee2fde71a7051503574a8ba564
Rookie_ki_ki <jisu7408@gmail.com>
2023-02-16 18:27:38 +0900

    [Add] 이름 기반 유저 검색 View Controller 레이아웃 작업 (#83)

---
+++ GetARock/GetARock/Screens/BandCreation/BandMemberAddFlow/UserSearchViewController.swift
@@ -0,0 +1,218 @@
+//
+//  UserSearchViewController.swift
+//  GetARock
+//
+//  Created by Jisu Jang on 2023/02/16.
+//
+
+import UIKit
+
+enum BottomScrollSection: Int {
+    case main
+}
+
+final class UserSearchViewController: BaseViewController {
+
+    private enum Size {
+        static let cellHeight: CGFloat = 36
+        static let cellContentInset: CGFloat = 50
+    }
+
+    private var tempWidth: CGFloat = 0
+
+    var completion: (_ selectedUsers: [MemberList]) -> Void = { selectedUsers in }
+
+    var selectedUsers: [MemberList] = []
+
+    private lazy var searchBar: SearchTextField = {
+        let searchBar = SearchTextField(placeholder: "닉네임으로 검색")
+        let action = UIAction { _ in
+            if searchBar.textField.text == "" {
+                self.searchResultTable.reloadData()
+            }
+        }
+        searchBar.textField.addAction(action, for: .editingChanged)
+        return searchBar
+    }()
+
+    private lazy var searchResultTable: UITableView = {
+        $0.delegate = self
+        $0.dataSource = self
+        $0.backgroundColor = .dark01
+        $0.register(UserSearchTableViewCell.self, forCellReuseIdentifier: UserSearchTableViewCell.classIdentifier)
+        $0.allowsMultipleSelection = true
+        return $0
+    }(UITableView())
+
+    private lazy var doneButton = {
+        let button = BasicButton(text: "완료", widthPadding: 200, heightPadding: 50)
+        let action = UIAction { _ in
+            self.dismiss(animated: true){
+                self.completion(self.selectedUsers)
+            }
+        }
+        button.addAction(action, for: .touchUpInside)
+        return button
+    }()
+
+    private lazy var selectedUserListScrollView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .dark01
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.delegate = self
+        collectionView.register(
+            AddedBandMemberCell.self,
+            forCellWithReuseIdentifier: AddedBandMemberCell.classIdentifier)
+        return collectionView
+    }()
+
+    //MARK: Bottom CollectionView
+    private lazy var bottomScrollViewDataSource: UICollectionViewDiffableDataSource<BottomScrollSection, MemberList> = self.makeDataSource()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLayout()
+        updateSnapShot(with: selectedUsers)
+    }
+
+    private func setupLayout() {
+        view.addSubview(searchBar)
+        searchBar.constraint(top: view.safeAreaLayoutGuide.topAnchor,
+                             leading: view.leadingAnchor,
+                             trailing: view.trailingAnchor,
+                             padding: UIEdgeInsets(top: 20, left: 25, bottom: 0, right: 25))
+
+        view.addSubview(doneButton)
+        doneButton.constraint(bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                              centerX: view.centerXAnchor,
+                              padding: UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0))
+
+        view.addSubview(selectedUserListScrollView)
+        selectedUserListScrollView.constraint(.heightAnchor, constant: 60)
+        selectedUserListScrollView.constraint(leading: view.safeAreaLayoutGuide.leadingAnchor,
+                                              bottom: doneButton.topAnchor,
+                                              trailing: view.safeAreaLayoutGuide.trailingAnchor,
+                                              padding: UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0))
+
+        view.addSubview(searchResultTable)
+        searchResultTable.constraint(top: searchBar.bottomAnchor,
+                                     leading: view.leadingAnchor,
+                                     bottom: selectedUserListScrollView.topAnchor,
+                                     trailing: view.trailingAnchor,
+                                     padding: UIEdgeInsets(top: 20, left: 25, bottom: 10, right: 25))
+
+    }
+}
+
+//MARK: TableView data configuration
+extension UserSearchViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        MemberDataDTO.testData.memberList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: UserSearchTableViewCell.classIdentifier,
+            for: indexPath) as? UserSearchTableViewCell else { return UITableViewCell()}
+        cell.configure(data: MemberDataDTO.testData.memberList[indexPath.row])
+        cell.selectionStyle = .none
+        return cell
+    }
+}
+//MARK: TableView delegate
+extension UserSearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 65
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        let selectedCell = tableView.cellForRow(at: indexPath) as! UserSearchTableViewCell
+        // 선택된 tableView의 데이터만 따로 추출
+        // 선택된 셀에 접근할 수 있으나 데이터는 따로 만들어야함
+        // CellInformation만들 때 임의의 id를 만들기 때문에, 만들고나서 선택한 cell의 id를 주입해줘야함
+        var data = MemberList(memberId: 0,
+                              name: selectedCell.titleLabel.text ?? "",
+                              memberState: "NONE",
+                              instrumentList: [InstrumentList(
+                                instrumentId: 0,
+                                isMain: true,
+                                name: selectedCell.subTitleLabel.text ?? "")])
+        // 선택될 때 Cell의 아이디 그대로 데이터에 넣기
+        data.id = selectedCell.id
+
+        print("This is index Path")
+        print(indexPath)
+
+        // collectionView Cell 크기 업데이트하기
+        //        tempWidth = data.nickName.size(withAttributes: [
+        //            .font : UIFont.preferredFont(forTextStyle: .subheadline)
+        //        ]).width + Size.cellContentInset
+
+        //MARK: 이미 배열에 들어가있는 셀 없애기
+        //TODO: 함수로 따로 빼서 만들기
+        selectedUsers.append(data)
+        selectedCell.isChecked = true
+        self.updateSnapShot(with: selectedUsers)
+    }
+
+    //MARK: Deselect Function
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let selectedCell = tableView.cellForRow(at: indexPath) as! UserSearchTableViewCell
+        selectedCell.isChecked = false
+        selectedUsers.removeAll { $0.id == selectedCell.id }
+        self.updateSnapShot(with: selectedUsers)
+    }
+}
+
+extension UserSearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //MARK: 동적 셀 크기 배정 코드 추후 추가 필요
+        let cellSize = CGSize(width: 100, height: 50)
+        return cellSize
+    }
+}
+
+extension UserSearchViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        self.searchBar.textField.becomeFirstResponder()
+    }
+}
+
+//MARK: SelectedUserScollView DiffableData Source
+extension UserSearchViewController {
+
+    func updateSnapShot(with items: [MemberList]) {
+        var snapShot = NSDiffableDataSourceSnapshot<BottomScrollSection, MemberList>()
+        snapShot.appendSections([.main])
+        snapShot.appendItems(items, toSection: .main)
+        self.bottomScrollViewDataSource.apply(snapShot, animatingDifferences: true)
+    }
+
+    func makeDataSource() -> UICollectionViewDiffableDataSource<BottomScrollSection, MemberList> {
+        return UICollectionViewDiffableDataSource<BottomScrollSection, MemberList>(collectionView: self.selectedUserListScrollView, cellProvider: { collectionView, indexPath, person in
+
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AddedBandMemberCell.classIdentifier, for: indexPath) as? AddedBandMemberCell else { return UICollectionViewCell() }
+
+            cell.configure(data: person)
+
+            let deleteAction = UIAction { _ in
+                self.selectedUsers.removeAll { $0.id == cell.id }
+                self.updateSnapShot(with: self.selectedUsers)
+
+                //TODO: Deselect 로직 수정 필요. 애초에 선택된 애들만 없앨 수 있게
+                for index in 0..<MemberDataDTO.testData.memberList.count {
+                    let searchResultTablecell = self.searchResultTable.cellForRow(at: IndexPath(row: index, section: 0)) as! UserSearchTableViewCell
+                    if searchResultTablecell.id == cell.id {
+                        searchResultTablecell.isChecked = false
+                    }
+                }
+            }
+            cell.deleteButton.addAction(deleteAction, for: .touchUpInside)
+            return cell
+        })
+    }
+}
