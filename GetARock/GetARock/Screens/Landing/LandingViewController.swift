b490d4e2619b913b0937eb612aa876620e42d6ec
ccdkk <cdk243@gmail.com>
2023-02-16 13:25:54 +0900

    [Del] bottomButton 삭제 (#105)

--- GetARock/GetARock/Screens/Landing/LandingViewController.swift
+++ GetARock/GetARock/Screens/Landing/LandingViewController.swift
@@ -8,9 +8,25 @@
 import UIKit
 
 class LandingViewController: UIViewController {
-
+    
+    private let button = UIButton(type: .system)
+    
      override func viewDidLoad() {
          super.viewDidLoad()
+         button.setTitle("모달", for: .normal)
+         view.addSubview(button)
+         button.constraint(centerX: view.centerXAnchor,
+                           centerY: view.centerYAnchor)
+         addAction()
+         
      }
-
+    
+    func addAction() {
+        let action = UIAction { _ in
+            let vc = ModifyMyPageViewController()
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: true)
+        }
+        button.addAction(action, for: .touchUpInside)
+    }
  }
