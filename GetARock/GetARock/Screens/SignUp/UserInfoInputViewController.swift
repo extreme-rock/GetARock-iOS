d73503937b760aedf10a450741ad6bd0c2c8155f
ccdkk <cdk243@gmail.com>
2023-02-17 17:58:13 +0900

    [Add]  UserInfoInputViewController 추가 (#143)

---
+++ GetARock/GetARock/Screens/SignUp/UserInfoInputViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UserInfoInputViewController.swift
+//  GetARock
+//
+//  Created by 최동권 on 2023/02/17.
+//
+
+import UIKit
+
+final class UserInfoInputViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
