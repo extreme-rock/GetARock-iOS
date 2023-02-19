96b65a362ced3ca4680363e31aab79f1494c9a96
ccdkk <cdk243@gmail.com>
2023-02-20 00:12:45 +0900

    [Del] 실험흔적 제거 (#105)

--- GetARock/GetARock/Screens/Landing/LandingViewController.swift
+++ GetARock/GetARock/Screens/Landing/LandingViewController.swift
@@ -35,27 +35,3 @@ class LandingViewController: UIViewController {
     }
  }
 
-class vc1: UIViewController {
-    
-    private let button = UIButton(type: .system)
-    
-     override func viewDidLoad() {
-         super.viewDidLoad()
-         button.setTitle("모달", for: .normal)
-         view.addSubview(button)
-         button.constraint(centerX: view.centerXAnchor,
-                           centerY: view.centerYAnchor)
-         addAction()
-         
-     }
-    
-    func addAction() {
-        let action = UIAction { [weak self] _ in
-            let vc = ModifyMyPageViewController()
-            vc.modalPresentationStyle = .fullScreen
-            self?.present(vc, animated: true)
-//            self?.navigationController?.pushViewController(vc, animated: true)
-        }
-        button.addAction(action, for: .touchUpInside)
-    }
-}
