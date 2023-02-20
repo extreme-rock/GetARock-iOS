449f1018e68172e36f57469c9b3a60200fd5fb58
compuTasha <mijookim.tasha@gmail.com>
2023-02-21 01:34:29 +0900

    [Remove] 불필요한 코드 삭제 (#107)

--- GetARock/GetARock/Screens/Setting/SettingViewVersionCell.swift
+++ GetARock/GetARock/Screens/Setting/SettingViewVersionCell.swift
@@ -55,11 +55,6 @@ class SettingViewVersionCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0))
-    }
-    
     // MARK: - Method
     
     private func attribute() {
