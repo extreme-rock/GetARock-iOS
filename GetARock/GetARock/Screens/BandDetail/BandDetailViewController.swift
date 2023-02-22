f844605c8f7ef648f48ab0df818d9bae0f12f4a3
GODNOEL <ah2650@naver.com>
2023-02-22 18:15:50 +0900

    [Add] BandData Get 함수 추가 (#78)

--- GetARock/GetARock/Screens/BandDetail/BandDetailViewController.swift
+++ GetARock/GetARock/Screens/BandDetail/BandDetailViewController.swift
@@ -12,7 +12,7 @@ final class BandDetailViewController: BaseViewController {
     // MARK: - Property
     
     //서버에서 Get하는 데이터 양식
-    private var getbandData = BandInformationVO(
+    private var bandData = BandInformationVO(
         bandID: 0,
         name: "",
         introduction: "",
@@ -26,24 +26,13 @@ final class BandDetailViewController: BaseViewController {
         songList: [],
         snsList: [],
         eventList: [],
-        commentList: [CommentList.init(commentID: 01,
-                                       memberName: "블랙로즈",
-                                       comment: "댓글내용입니당",
-                                       createdDate: "2022.11.19 13:20"),
-                      CommentList.init(commentID: 02,
-                                       memberName: "오아시스",
-                                       comment: "오 효자동 근처 밴드네요! 반갑습니당 >///< 저희도 근처에 있는데 꼭 공연보러갈게요!",
-                                       createdDate: "2022.11.19 13:20"),
-                      CommentList.init(commentID: 03,
-                                       memberName: "3번쨰 밴드",
-                                       comment: "오 효자동 근처 밴드네요! 반갑습니당 >///< 저희도 근처에 있는데 꼭 공연보러갈게요!",
-                                       createdDate: "2022.11.19 13:20")]
+        commentList: []
     )
     
     // MARK: - View
     
     lazy var bandTopInfoView = BandTopInfoView()
-    lazy var bandDetailContentView = DetailContentView(type: .band, bandData: getbandData)
+    lazy var bandDetailContentView = DetailContentView(type: .band, bandData: bandData)
     
     // MARK: - LifeCycle
     
@@ -52,7 +41,9 @@ final class BandDetailViewController: BaseViewController {
         
         //비동기 테스크가 만들어짐 -> 비동기함수가 아닌거에 비동기함수를 넣어야할때
         Task{
-            
+            await getBandData()
+            print("가져오기 성공")
+            print(bandData)
         }
         
         view.addSubview(bandTopInfoView)
@@ -71,7 +62,29 @@ final class BandDetailViewController: BaseViewController {
             trailing: self.view.trailingAnchor,
             padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         )
-        
-        
+    }
+}
+
+extension BandDetailViewController {
+    
+    func getBandData() async {
+        var queryURLComponent = URLComponents(string: "http://43.201.55.66:8080/band")
+        let idQuery = URLQueryItem(name: "id", value: "1")
+        queryURLComponent?.queryItems = [idQuery]
+        guard let url = queryURLComponent?.url else { return }
+
+        do {
+            //MARK: 데이터 받아오기
+            let (data, response) = try await URLSession.shared.data(from: url)
+
+            //MARK: 데이터 디코딩
+            let decodedData = try JSONDecoder().decode(BandInformationVO.self, from: data)
+                print("Response data raw: \(data)")
+                print("fetch Data")
+                print("\(response)")
+                self.bandData = decodedData
+        } catch {
+            print("bad news! decoding error occuerd")
+        }
     }
 }
