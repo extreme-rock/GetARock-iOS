3a6ac71ed7759adbd11335016d43d548afc86de5
ccdkk <cdk243@gmail.com>
2023-02-23 14:01:06 +0900

    [Style] font 변경 & 네이밍 변경 (#102)

--- GetARock/GetARock/Screens/ModifyMyPage/ModifyUserProfileViewController.swift
+++ GetARock/GetARock/Screens/ModifyMyPage/ModifyUserProfileViewController.swift
@@ -43,7 +43,7 @@ final class ModifyUserProfileViewController: UIViewController {
     
     private let userNamingGuideSubLabel = BasicLabel(
         contentText: "* 공백없이 20자 이하, 기호는 _만 입력 가능합니다.",
-        fontStyle: .content,
+        fontStyle: .caption,
         textColorInfo: .gray02)
 
     private lazy var userNamingTextField: TextLimitTextField = TextLimitTextField(
@@ -52,7 +52,7 @@ final class ModifyUserProfileViewController: UIViewController {
         duplicationCheckType: .userName,
         textExpressionCheck: true)
     
-    private lazy var textFieldStackView: UIStackView = {
+    private lazy var userNameInputStackView: UIStackView = {
         $0.axis = .vertical
         $0.spacing = 10
         return $0
@@ -102,7 +102,7 @@ final class ModifyUserProfileViewController: UIViewController {
         maxCount: 300
     )
 
-    private lazy var textViewStackView: UIStackView = {
+    private lazy var userIntroStackView: UIStackView = {
         $0.axis = .vertical
         $0.spacing = 10
         return $0
@@ -113,13 +113,13 @@ final class ModifyUserProfileViewController: UIViewController {
 
     private let snsFirstSubTitleLabel = BasicLabel(
         contentText: "* 본인의 SNS 계정을 입력해주세요.",
-        fontStyle: .content,
+        fontStyle: .caption,
         textColorInfo: .gray02
     )
 
     private let snsSecondSubTitleLabel = BasicLabel(
         contentText: "* 본인계정이 아닌 계정 등록 시 책임은 본인에게 있습니다.",
-        fontStyle: .content,
+        fontStyle: .caption,
         textColorInfo: .gray02
     )
     
@@ -133,9 +133,7 @@ final class ModifyUserProfileViewController: UIViewController {
                                      snsSecondSubTitleLabel]))
 
     private let youtubeTextField = SNSBoxView(type: .youTube, placeholder: "채널명")
-
     private let instagramTextField = SNSBoxView(type: .instagram, placeholder: "사용자 계정")
-
     private let soundCloudTextField = SNSBoxView(type: .soundCloud, placeholder: "사용자 계정")
 
     private lazy var snsInformationStackView: UIStackView = {
@@ -169,10 +167,10 @@ final class ModifyUserProfileViewController: UIViewController {
         $0.backgroundColor = .dark01
         return $0
     }(UIStackView(arrangedSubviews: [titleStackView,
-                                     textFieldStackView,
+                                     userNameInputStackView,
                                      ageInputStackView,
                                      genderInputStackView,
-                                     textViewStackView,
+                                     userIntroStackView,
                                      snsInformationStackView,
                                      informationFillCompleteButton]))
     
