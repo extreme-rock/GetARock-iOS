ed0271f534d24a80a0078b72b30df2dede1c07fd
ccdkk <cdk243@gmail.com>
2023-02-24 23:00:24 +0900

    [Add] SignUpNetworkManager 추가 (#132)

---
+++ GetARock/GetARock/Network/SignUpNetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpNetworkManager.swift
+//  GetARock
+//
+//  Created by 최동권 on 2023/02/24.
+//
+
+import Foundation
+
+final class SignUpNetworkManager {
+    static func postMember(user: User) {
+        
+    }
+}
