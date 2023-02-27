a83c388e9d1de3f24fca93db6ec868cc2620de29
compuTasha <mijookim.tasha@gmail.com>
2023-02-27 15:44:44 +0900

    [Add] 앱 런치 시 권한 요청 함수 호출 (#142)

--- GetARock/GetARock/Global/Supports/AppDelegate.swift
+++ GetARock/GetARock/Global/Supports/AppDelegate.swift
@@ -13,8 +13,8 @@ import GoogleMaps
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         GMSServices.provideAPIKey(Bundle.main.apiKey)
+        registerForRemoteNotifications()
         
         return true
     }
@@ -33,6 +33,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
     
+}
+
 extension AppDelegate {
     private func registerForRemoteNotifications() {
         let center = UNUserNotificationCenter.current()
