9ce216a0afe034050b03d3a4c67c98d986fc37a4
compuTasha <mijookim.tasha@gmail.com>
2023-02-27 15:37:23 +0900

    [Add] DeviceTokenDTO 추가 (#142)

---
+++ GetARock/GetARock/Network/Model/DeviceTokenDTO.swift
@@ -0,0 +1,13 @@
+//
+//  DeviceTokenDTO.swift
+//  GetARock
+//
+//  Created by Mijoo Kim on 2023/02/27.
+//
+
+import Foundation
+
+struct DeviceTokenDTO: Codable {
+    let memberID: Int
+    let deviceToken: String
+}
