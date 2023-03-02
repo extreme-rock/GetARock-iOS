d438bcdcaae7dcd0e04ef518b12a20fe81af1576
Jay <103009135+Rookie0031@users.noreply.github.com>
2023-03-02 22:00:39 +0900

    [Add] 이미지 리터럴 및 이미지 파일 추가 (#97)

--- GetARock/GetARock/Global/Literal/ImageLiteral.swift
+++ GetARock/GetARock/Global/Literal/ImageLiteral.swift
@@ -28,8 +28,8 @@ enum ImageLiteral {
 
     static var bellSymbol: UIImage { .load(systemName: "bell.fill") }
     static var checkMarkCircleFillSymbol: UIImage { .load(systemName: "checkmark.circle.fill") }
-
-
+    static var chatBubbleSymbol: UIImage { .load(systemName: "bubble.left.and.bubble.right.fill") }
+    static var clockArrowCirclePathSymbol: UIImage { .load(systemName: "clock.arrow.circlepath") }
 
     static var exclamationMarkCircleSymbol: UIImage { .load(systemName: "exclamationmark.circle") }
 
@@ -47,6 +47,7 @@ enum ImageLiteral {
     static var customPinIcon: UIImage { .load(named: "customPinIcon") }
     static var eventNotificationIcon: UIImage { .load(named: "eventNotificationIcon") }
     static var bandNotificationIcon: UIImage { .load(named: "bandNotificationIcon") }
+    static var bubbleWithThunderIcon: UIImage { .load(named: "bubbleWithThunderIcon") }
 }
 
 extension UIImage {
