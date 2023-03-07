93b05635e81554c36e14192f0c153142d3acb445
Jay <103009135+Rookie0031@users.noreply.github.com>
2023-03-07 23:21:59 +0900

    [Design] 미가입 멤버 추가 스택뷰 레이아웃 일부 수정

--- GetARock/GetARock/Screens/BandCreation/BandMemberAddFlow/UnRegisteredMemberAdd/UnRegisteredMemberCardView.swift
+++ GetARock/GetARock/Screens/BandCreation/BandMemberAddFlow/UnRegisteredMemberAdd/UnRegisteredMemberCardView.swift
@@ -29,15 +29,25 @@ final class UnRegisteredMemberCardView: UIStackView, Identifiable {
     }()
 
     //TODO: develop Pull후에 Information Guide Label로 업데이트 필요
-    private let nickNameGuideLabel = BasicLabel(contentText: "닉네임", fontStyle: .headline01, textColorInfo: .white)
+    private let nickNameGuideLabel = BasicLabel(contentText: "닉네임",
+                                                fontStyle: .headline01,
+                                                textColorInfo: .white)
 
-    let nickNameTextField = TextLimitTextField(placeholer: "닉네임을 입력해주세요", maxCount: 10, duplicationCheckType: .none, textExpressionCheck: true)
+    let nickNameTextField = {
+        $0.constraint(.widthAnchor,
+                      constant: BasicComponentSize.width - 36)
+        return $0
+    }(TextLimitTextField(placeholer: "닉네임을 입력해주세요",
+                                               maxCount: 10,
+                                               duplicationCheckType: .none,
+                                               textExpressionCheck: true))
 
     private lazy var nickNameVstack: UIStackView = {
         $0.axis = .vertical
         $0.spacing = 10
         return $0
-    }(UIStackView(arrangedSubviews: [nickNameGuideLabel, nickNameTextField]))
+    }(UIStackView(arrangedSubviews: [nickNameGuideLabel,
+                                     nickNameTextField]))
 
     //TODO: develop Pull후에 Information Guide Label로 업데이트 필요
     private let mainPositionGuideLabel = BasicLabel(contentText: "포지션", fontStyle: .headline01, textColorInfo: .white)
@@ -45,25 +55,41 @@ final class UnRegisteredMemberCardView: UIStackView, Identifiable {
     let positionSelectCollectionView: SelectCollectionView = {
         $0.constraint(.heightAnchor, constant: 110)
         return $0
-    }(SelectCollectionView(widthState: .fixed, items: ["보컬", "기타", "베이스", "드럼", "키보드"], widthSize: 100, itemSpacing: 7))
+    }(SelectCollectionView(widthState: .fixed, items: ["보컬", "기타", "베이스", "드럼", "키보드"],
+                           widthSize: 100,
+                           itemSpacing: 7))
 
     private lazy var positionSelectVstack: UIStackView = {
         $0.axis = .vertical
         $0.spacing = 10
         return $0
-    }(UIStackView(arrangedSubviews: [mainPositionGuideLabel, positionSelectCollectionView]))
+    }(UIStackView(arrangedSubviews: [mainPositionGuideLabel,
+                                     positionSelectCollectionView]))
 
-    private let otherPositionGuideLabel = BasicLabel(contentText: "그 외 포지션", fontStyle: .headline01, textColorInfo: .white)
+    private let otherPositionGuideLabel = BasicLabel(contentText: "그 외 포지션",
+                                                     fontStyle: .headline01,
+                                                     textColorInfo: .white)
 
-    private let othrePositionSubGuideLabel = BasicLabel(contentText: "* 그 외 포지션은 공백 포함 10자 이하로 입력가능합니다.", fontStyle: .content, textColorInfo: .gray02)
+    private let otherPositionSubGuideLabel = BasicLabel(
+        contentText: "* 그 외 포지션은 공백 포함 10자 이하로 입력가능합니다.",
+        fontStyle: .content,
+        textColorInfo: .gray02)
 
-    let otherPositionTextField = TextLimitTextField(placeholer: "그 외 포지션을 입력해주세요.", maxCount: 10, duplicationCheckType: .none, textExpressionCheck: true)
+    let otherPositionTextField = {
+        $0.constraint(.widthAnchor,
+                      constant: BasicComponentSize.width - 36)
+        return $0
+    }(TextLimitTextField(placeholer: "그 외 포지션을 입력해주세요.",
+                                                    maxCount: 10, duplicationCheckType: .none,
+                                                    textExpressionCheck: true))
 
     private lazy var otherPositionVstack: UIStackView = {
         $0.axis = .vertical
         $0.spacing = 10
         return $0
-    }(UIStackView(arrangedSubviews: [otherPositionGuideLabel, othrePositionSubGuideLabel, otherPositionTextField]))
+    }(UIStackView(arrangedSubviews: [otherPositionGuideLabel,
+                                     otherPositionSubGuideLabel,
+                                     otherPositionTextField]))
 
     init() {
         super.init(frame: .zero)
@@ -77,11 +103,13 @@ final class UnRegisteredMemberCardView: UIStackView, Identifiable {
         self.addArrangedSubview(otherPositionVstack)
         self.axis = .vertical
         self.spacing = 40
-        self.layoutMargins = UIEdgeInsets(top: 30, left: 16, bottom: 30, right: 16)
         self.isLayoutMarginsRelativeArrangement = true
+        self.layoutMargins = UIEdgeInsets(top: 30, left: 20, bottom: 30, right: 20)
 
         self.addSubview(cancelButton)
-        cancelButton.constraint(top: self.topAnchor, trailing: self.trailingAnchor, padding: UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 10))
+        cancelButton.constraint(top: self.topAnchor,
+                                trailing: self.trailingAnchor,
+                                padding: UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 10))
     }
 
     private func attribute() {
