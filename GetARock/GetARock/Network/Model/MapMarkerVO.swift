3e2365a37199ee1be16a8743527592fb1ac001f3
compuTasha <mijookim.tasha@gmail.com>
2023-03-08 19:38:11 +0900

    [Add] MapMarkerVO 추가 (#112)

---
+++ GetARock/GetARock/Network/Model/MapMarkerVO.swift
@@ -0,0 +1,34 @@
+//
+//  MapMarkerVO.swift
+//  GetARock
+//
+//  Created by Mijoo Kim on 2023/03/07.
+//
+
+import Foundation
+import MapKit
+
+struct MapMarkerVO: Codable {
+    let bandList: [BandMarkerVO]
+    let eventList: [EventMarkerVO]
+}
+
+struct BandMarkerVO: Codable {
+    let id: Int
+    let name: String
+    let longitude: Double
+    let latitude: Double
+}
+
+struct EventMarkerVO: Codable {
+    let id: Int
+    let name: String
+    let longitude: Double
+    let latitude: Double
+}
+
+extension BandMarkerVO {
+    func toCLLocationCoordinate2D() -> CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: self.latitude, longitude: self.longitude)
+    }
+}
