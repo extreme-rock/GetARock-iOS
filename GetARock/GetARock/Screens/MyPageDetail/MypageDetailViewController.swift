755ba144b1432287055df649f667e0085f2900a2
GODNOEL <ah2650@naver.com>
2023-03-19 17:30:04 +0900

    [Add] test Data 추가 (#90)

--- GetARock/GetARock/Screens/MyPageDetail/MypageDetailViewController.swift
+++ GetARock/GetARock/Screens/MyPageDetail/MypageDetailViewController.swift
@@ -12,15 +12,41 @@ final class MypageDetailViewController: UIViewController {
     // MARK: - Property
     
     //TODO: - 추후 상세페이지의 멤버 아이디를 지도로부터 받아와야함
-    private var userID = "22"
+    private var userID = "18"
+    //    private var userData = UserInformationVO(
+    //        userID: 0,
+    //        name: "",
+    //        age: "",
+    //        gender: "",
+    //        introduction: nil,
+    //        bandList: nil,
+    //        instrumentList: [],
+    //        snsList: nil,
+    //        eventList: nil,
+    //        commentEventList: nil)
     private var userData = UserInformationVO(
         userID: 0,
-        name: "",
-        age: "",
-        gender: "",
-        introduction: nil,
-        bandList: nil,
-        instrumentList: [],
+        name: "노엘노엘노엘노엘노엘노엘노엘노엘노엘노엘노엘노엘노엘노엘",
+        age: "TWENTIES",
+        gender: "MAN",
+        introduction: "안녕하세요 노엘입니다.",
+        bandList: [BandListVO(bandID: 10,
+                              name: "극 - 락",
+                              memberCount: 6,
+                              memberAge: "20-60대"),
+                   BandListVO(bandID: 12,
+                              name: "모여락",
+                              memberCount: 6,
+                              memberAge: "20-60대")],
+        instrumentList: [InstrumentListVO(instrumentID: 1,
+                                          isMain: true,
+                                          name: "guitar"),
+                         InstrumentListVO(instrumentID: 2,
+                                          isMain: false,
+                                          name: "bass"),
+                         InstrumentListVO(instrumentID: 3,
+                                          isMain: false,
+                                          name: "캐스터네츠")],
         snsList: nil,
         eventList: nil,
         commentEventList: nil)
@@ -33,14 +59,15 @@ final class MypageDetailViewController: UIViewController {
         userAge: userData.age
     )
     
+    private lazy var userInfomationView = UserInformationView(userData: userData)
+    
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .purple
         Task {
-            await fetchUserData()
-            print(userData)
+            //await fetchUserData()
             setupLayout()
         }
     }
@@ -55,8 +82,6 @@ final class MypageDetailViewController: UIViewController {
             trailing: self.view.trailingAnchor
         )
     }
-    
-    
 }
 
 // MARK: - fetchBandData
