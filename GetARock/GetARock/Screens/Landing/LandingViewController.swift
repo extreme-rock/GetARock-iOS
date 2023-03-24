10a40d08133f6f7069d2295b31450bb66d078dd7
compuTasha <mijookim.tasha@gmail.com>
2023-03-24 17:52:48 +0900

    [Add] 랜딩페이지 백그라운드 이미지 설정 (#114)

--- GetARock/GetARock/Screens/Landing/LandingViewController.swift
+++ GetARock/GetARock/Screens/Landing/LandingViewController.swift
@@ -8,7 +8,26 @@
 import UIKit
 
 class LandingViewController: UIViewController {
-     override func viewDidLoad() {
-         super.viewDidLoad()
+    
+    // MARK: - Property
+    
+    private let backgroundImageView: UIImageView = {
+        $0.image = ImageLiteral.landingBackground
+        $0.contentMode = .scaleAspectFill
+        return $0
+    }(UIImageView(frame: UIScreen.main.bounds))
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLayout()
+    }
+    
+    // MARK: - Methods
+    
+    private func setupLayout() {
+        self.view.insertSubview(backgroundImageView, at: 0)
+    }
      }
  }
