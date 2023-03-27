ccb50d6b2e3ed6d1c85d8fc8d13d0ed5371efca0
compuTasha <mijookim.tasha@gmail.com>
2023-03-27 23:43:06 +0900

    [Add] User Default 관련 파일 추가 (#114)

---
+++ GetARock/GetARock/Network/Storage/UserDefaultStorage.swift
@@ -0,0 +1,65 @@
+//
+//  UserDefaultStorage.swift
+//  GetARock
+//
+//  Created by Mijoo Kim on 2023/03/25.
+//
+
+import Foundation
+
+enum DataKeys: String, CaseIterable {
+    case isLogin = "isLogin"
+    case userID = "userID"
+    case memberID = "memberID"
+    case accessToken = "accessToken"
+    case refreshToken = "refreshToken"
+}
+
+struct UserDefaultStorage {
+    static var isLogin: Bool {
+        return UserData<Bool>.getValue(forKey: .isLogin) ?? false
+    }
+    
+    static var userID: String {
+        return UserData<String>.getValue(forKey: .userID) ?? ""
+    }
+    
+    static var memberID: Int {
+        return UserData<Int>.getValue(forKey: .memberID) ?? 0
+    }
+    
+    static var accessToken: String {
+        return UserData<String>.getValue(forKey: .accessToken) ?? ""
+    }
+    
+    static var refreshToken: String {
+        return UserData<String>.getValue(forKey: .refreshToken) ?? ""
+    }
+    
+}
+
+struct UserData<T> {
+    static func getValue(forKey key: DataKeys) -> T? {
+        if let data = UserDefaults.standard.value(forKey: key.rawValue) as? T {
+            return data
+        } else {
+            return nil
+        }
+    }
+    
+    static func setValue(_ value: T, forKey key: DataKeys) {
+        UserDefaults.standard.set(value, forKey: key.rawValue)
+    }
+    
+    static func clearAll() {
+        DataKeys.allCases.forEach { key in
+            print(key.rawValue)
+            UserDefaults.standard.removeObject(forKey: key.rawValue)
+        }
+    }
+    
+    static func clear(forKey key: DataKeys) {
+        UserDefaults.standard.removeObject(forKey: key.rawValue)
+    }
+    
+}
