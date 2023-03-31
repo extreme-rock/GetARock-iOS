ccb6e99a72a471e78f7b4290d8dbe6fb46278d9f
compuTasha <mijookim.tasha@gmail.com>
2023-03-31 23:54:55 +0900

    [Add] DeleteAccountVO 추가 (#110)

---
+++ GetARock/GetARock/Network/Model/DeleteAccountVO.swift
@@ -0,0 +1,13 @@
+//
+//  DeleteAccountVO.swift
+//  GetARock
+//
+//  Created by Mijoo Kim on 2023/03/31.
+//
+
+import Foundation
+
+struct DeleteAccountVO: Codable {
+    let id: Int?
+    let success: Bool
+}
